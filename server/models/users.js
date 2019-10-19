cb35f5ed15c838721097380e6f2b13bad1615fe0
StuartDambi <stuartdambi@gmail.com>
2019-10-19 18:58:40 +0300

    added email validations

--- server/models/users.js
+++ server/models/users.js
@@ -4,7 +4,10 @@ const userSchema = mongoose.Schema({
   _id: mongoose.Types.ObjectId,
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  email: { type: String, required: true },
+  email: {
+    // eslint-disable-next-line no-useless-escape
+    type: String, required: true, unique: true, match: /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
+  },
   telephone: { type: String, required: true },
   fax: { type: String, required: false },
   company: { type: String, required: false },
