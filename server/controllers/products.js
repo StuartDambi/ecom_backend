4dbc8ed638e13c0729db7385ae0dbd8dcacc2e3c
StuartDambi <stuartdambi@gmail.com>
2019-10-22 18:23:25 +0300

    added the delete and update product APIs

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import Product from '../models/products';
 
 const productController = {
+  // Get all products
   getProducts: (req, res) => {
     Product.find((err, docs) => {
       if (err) {
@@ -15,6 +16,32 @@ const productController = {
         docs,
       });
     });
+  },
+  //   Get one Product
+  getProduct: (req, res) => {
+    const id = req.params.productId;
+    Product.findById(id).exec()
+      .then((doc) => {
+        if (doc) {
+          res.status(200).json({
+            status: res.statusCode,
+            doc,
+          });
+        } else if (!doc) {
+          return res.status(400).json({
+            status: res.statusCode,
+            message: 'Product doesn\'t exist',
+          });
+        }
+        return res.status(404).json({
+          status: res.statusCode,
+          message: 'Please check that the Id entered has a correct syntax',
+        });
+      })
+      .catch((error) => res.status(500).json({
+        status: res.statusCode,
+        error,
+      }));
   },
   //   Add Products
   addProducts: (req, res) => {
@@ -37,6 +64,55 @@ const productController = {
       }))
       .catch((error) => res.status(500).json({
         status: res.statusCode,
+        error,
+      }));
+  },
+  //   Delete a Product
+  deleteProduct: (req, res) => {
+    const id = req.params.productId;
+    Product.deleteOne({ _id: id })
+      .exec()
+      .then((result) => {
+        if (result) {
+          res.status(200).json({
+            status: res.statusCode,
+            message: 'Products has been deleted',
+            result,
+          });
+        }
+        return res.status(404).json({
+          status: res.statusCode,
+          message: 'Product does not exist',
+        });
+      })
+      .catch((error) => res.status(500).json({
+        message: 'Something went wrong',
+        error,
+      }));
+  },
+  //   Update the Product
+  updateProduct: (req, res) => {
+    const id = req.params.productId;
+    const updateOps = {};
+    // eslint-disable-next-line no-restricted-syntax
+    for (const ops of req.body) {
+      updateOps[ops.propName] = ops.value;
+    }
+    Product.updateOne({ _id: id }, { $set: updateOps })
+      .exec()
+      .then((result) => {
+        if (result) {
+          return res.status(200).json({
+            status: res.statusCode,
+            result,
+          });
+        }
+        return res.status(404).json({
+          status: res.statusCode,
+          message: 'Product does not exist',
+        });
+      })
+      .catch((error) => res.status(500).json({
         error,
       }));
   },
--- server/routes/products.js
+++ server/routes/products.js
@@ -6,7 +6,16 @@ const router = express.Router();
 // Get the Products
 router.get('/', productController.getProducts);
 
+// Get single Product
+router.get('/:productId', productController.getProduct);
+
 // Add Products
 router.post('/', productController.addProducts);
+
+// Update a Product
+router.patch('/:productId', productController.updateProduct);
+
+// Delete a product
+router.delete('/:productId', productController.deleteProduct);
 
 module.exports = router;
